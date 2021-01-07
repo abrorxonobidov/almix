407e6c388fda381cff08f7dd6a56c426c293ace3
abrorxonobidov <abrorkhon.obidov@gmail.com>
2021-01-07 05:57:31 +0500

    migration fix

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -15,15 +15,27 @@ class m130524_201442_init extends Migration
         $this->createTable('{{%user}}', [
             'id' => $this->primaryKey(),
             'username' => $this->string()->notNull()->unique(),
+            'full_name' => $this->string(),
             'auth_key' => $this->string(32)->notNull(),
             'password_hash' => $this->string()->notNull(),
             'password_reset_token' => $this->string()->unique(),
             'email' => $this->string()->notNull()->unique(),
 
             'status' => $this->smallInteger()->notNull()->defaultValue(10),
-            'created_at' => $this->integer()->notNull(),
-            'updated_at' => $this->integer()->notNull(),
+            'created_at' => $this->integer(),
+            'updated_at' => $this->integer(),
         ], $tableOptions);
+
+        $this->createIndex('user_id_uindex', '{{%user}}', 'id', true);
+
+        $this->insert('{{%user}}', [
+            'username' => 'abrorxonobidov',
+            'full_name' => 'Abrorxon Obidov',
+            'auth_key' => 'oyXF-L0BTDbFHkDkCQ5EKSM5mwv5EPbb',
+            'password_hash' => '$2y$13$i4SsfJqtpINqR08pZauVIONeMAczA9f2jJgQhZCrtEEojnKJq34H2',
+            'email' => 'abrorkhon.obidov@gmail.com',
+            'status' => 10
+        ]);
     }
 
     public function down()
